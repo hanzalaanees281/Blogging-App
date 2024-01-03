09ff3fd3c897d18485afc6383750d9443d92bbc1
hanzalaanees281 <hanzalaanees555@gmail.com>
2024-01-04 02:58:10 +0500

    Add login and signup components and create custom components

--- BloggingApp/src/components/index.js
+++ BloggingApp/src/components/index.js
@@ -3,7 +3,10 @@ import Footer from "./Footer/Footer";
 import Container from "./container/Container";
 import Logo from "./Logo";
 import LogoutBtn from "./Header/LogoutBtn"
-
+import Input from "./Input"
+import Button from "./Button"
+import Select from "./Select"
+import PostCard from "./PostCard"
 export {
-    Header, Footer, Container, Logo, LogoutBtn
+    Header, Footer, Container, Logo, LogoutBtn, Input, Button, PostCard, Select
 }
